3eb5e0a5d79ffb9618c99db275361769c78df9c8
carlota-home <carlotamenendez96@hotmail.es>
2023-02-26 16:59:40 +0100

    a

--- Script/paginaDetalleScript.js
+++ Script/paginaDetalleScript.js
@@ -1,14 +1,14 @@
 window.onload=function(){
   //Referencias
     document.querySelector(".navegador__atras").addEventListener("click",()=>window.location.href="pagina.html");
-    var texto = document.querySelector(".contador__texto");
+    
 
     var cuerpo = document.querySelector(".cuerpo");
       //DATOS RECUPERADOS DEL SESSIONSTORAGE
     var perfil = JSON.parse(sessionStorage.getItem("llavero"));
     pintaDatos(perfil);
   
-    console.log(perfil.nombre);
+    
 //Funcion que pintará la foto y el cuerpo
     function pintaDatos(perfil){
         var imagen = document.createElement("img");
@@ -47,14 +47,10 @@ window.onload=function(){
         cuerpo.appendChild(contenedor);
     }
 
-    //Evento para el boton de comprar
-    boton.addEventListener("click",añadeCarrito);
-
-    function añadeCarrito(){
-      //Añadire al carrito la variable perfil donde se encuentra el objeto que añado y ademas debo guardar la cantidad que se va a comprar
-    }
+   
       
     //Eventos y funciones de los botones de decremento y aumento
+    var texto = document.querySelector(".contador__texto");
     document.querySelector(".contador__cremento").addEventListener("click",inc);
     document.querySelector(".contador__decremento").addEventListener("click",dec);
       function inc() {
@@ -65,5 +61,13 @@ window.onload=function(){
           texto.value = parseInt(texto.value) - 1;
         }
       }
+
+
+       //Evento para el boton de comprar
+    boton.addEventListener("click",añadeCarrito);
+
+    function añadeCarrito(){
+      //Añadire al carrito la variable perfil donde se encuentra el objeto que añado y ademas debo guardar la cantidad que se va a comprar
+    }
 
 }
